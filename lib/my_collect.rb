6456f8dd2304c57e85115cfb65a46cf813eed31f
ANDREW PHAVISETH <andrewphaviseth@gmail.com>
2019-05-23 03:48:29 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(array)
   i = 0 
   student_names = [ ]
   while i < array.length 
-    student_names << yield array [i]
+    student_names << yield(array [i])
     i += 1 
   end
   student_names
